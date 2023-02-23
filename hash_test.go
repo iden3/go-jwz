1a6b84f67b040570217144150ccffba5c5a372ab
Oleksandr Brezhniev <oleksandr.brezhniev@gmail.com>
2023-02-23 23:04:59 +0200

    require.NoError instead of assert

--- hash_test.go
+++ hash_test.go
@@ -3,6 +3,8 @@ package jwz
 import (
 	"github.com/iden3/go-iden3-crypto/poseidon"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
 	"math/big"
 	"testing"
 )
@@ -12,7 +14,7 @@ func TestHash(t *testing.T) {
 	msg := "message"
 
 	h, err := Hash([]byte(msg))
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	assert.Equal(t, h.String(), "12195879903067908640854440056941289904003404799313352286287749481941648225513")
 }
@@ -23,7 +25,7 @@ func TestPoseidonHash(t *testing.T) {
 	toHash := new(big.Int).SetBytes([]byte(msg))
 
 	i, err := poseidon.Hash([]*big.Int{toHash})
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, i.String(), "16076885786305451396952367807583087877643965039481491647404584414044042908412")
 
 }
--- jwz_test.go
+++ jwz_test.go
@@ -10,6 +10,7 @@ import (
 	core "github.com/iden3/go-iden3-core"
 	"github.com/iden3/go-rapidsnark/types"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func MockPrepareAuthInputs(_ []byte, _ circuits.CircuitID) ([]byte, error) {
@@ -25,7 +26,7 @@ func MockPrepareAuthV2Inputs(_ []byte, _ circuits.CircuitID) ([]byte, error) {
 func TestNewWithPayload(t *testing.T) {
 	payload := []byte("mymessage")
 	token, err := NewWithPayload(ProvingMethodGroth16AuthV2Instance, payload, MockPrepareAuthV2Inputs)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	assert.Equal(t, "groth16", token.Alg)
 	assert.Equal(t, "authV2", token.CircuitID)
@@ -37,28 +38,25 @@ func TestToken_Prove(t *testing.T) {
 
 	payload := []byte("mymessage")
 	token, err := NewWithPayload(ProvingMethodGroth16AuthV2Instance, payload, MockPrepareAuthV2Inputs)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	var provingKey, verificationKey, wasm []byte
 
 	provingKey, err = os.ReadFile("./testdata/authV2/circuit_final.zkey")
-	assert.Nil(t, err)
+	require.NoError(t, err)
 
 	wasm, err = os.ReadFile("./testdata/authV2/circuit.wasm")
-	assert.Nil(t, err)
+	require.NoError(t, err)
 
 	verificationKey, err = os.ReadFile("./testdata/authV2/verification_key.json")
-	assert.Nil(t, err)
-
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	tokenString, err := token.Prove(provingKey, wasm)
-
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	t.Log(tokenString)
 
 	isValid, err := token.Verify(verificationKey)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.True(t, isValid)
 
 }
@@ -66,25 +64,25 @@ func TestToken_Prove(t *testing.T) {
 func BenchmarkToken_Prove(b *testing.B) {
 	payload := []byte("mymessage")
 	token, err := NewWithPayload(ProvingMethodGroth16AuthV2Instance, payload, MockPrepareAuthV2Inputs)
-	assert.NoError(b, err)
+	require.NoError(b, err)
 
 	var provingKey, verificationKey, wasm []byte
 
 	provingKey, err = os.ReadFile("./testdata/authV2/circuit_final.zkey")
-	assert.Nil(b, err)
+	require.NoError(b, err)
 
 	wasm, err = os.ReadFile("./testdata/authV2/circuit.wasm")
-	assert.Nil(b, err)
+	require.NoError(b, err)
 
 	verificationKey, err = os.ReadFile("./testdata/authV2/verification_key.json")
-	assert.NoError(b, err)
+	require.NoError(b, err)
 
 	for i := 0; i < b.N; i++ {
 		_, err := token.Prove(provingKey, wasm)
-		assert.NoError(b, err)
+		require.NoError(b, err)
 
 		isValid, err := token.Verify(verificationKey)
-		assert.NoError(b, err)
+		require.NoError(b, err)
 		assert.True(b, isValid)
 	}
 }
@@ -92,16 +90,16 @@ func BenchmarkToken_Prove(b *testing.B) {
 func TestToken_Parse(t *testing.T) {
 
 	token, err := Parse("eyJhbGciOiJncm90aDE2IiwiY2lyY3VpdElkIjoiYXV0aFYyIiwiY3JpdCI6WyJjaXJjdWl0SWQiXSwidHlwIjoiSldaIn0.bXltZXNzYWdl.eyJwcm9vZiI6eyJwaV9hIjpbIjE5MTU5MDg5MTAwMDkzNDQyMzY0NTY0MjQxOTA3ODQ1MzkxODgxMzM5NDQ3NDkxNTcwNjg2NTk5NDE3MjA0MzUwNTE1ODE0NzYxNDE1IiwiNDQ4MDg2MzgzNDY4MTU2ODM2MTI2NTI1NzgzMzkyMjk1OTE1Mzg5OTQwNDUzMDkxNjcxNTA5NjEyMzg3NTU1MzY0NjM3NjMwNTQzOSIsIjEiXSwicGlfYiI6W1siMTA3MjY0OTYxNTk4OTQwNDAyNTExMDYyMDkyOTA5MjUzOTQ3MDU1MTk0NTYyNTkyMDYwNjgxMTE0MTY4ODQyMDI2MzI0MzY4Nzk1MDAiLCIzODkwMTY0OTc1OTMzOTQzMDY2NTc5ODI3OTk2MDcxNzI0NDg5NjEwNDU1ODQ0NTU5NDQ2MDIwMTk4ODQyNDQwNzk5MzAyNzQyOTk5Il0sWyIxOTY4NjI5MDk3ODAzMzI1MTU1MjczMjAzNTMxMzIyODYwNTE0Mzc3OTUwOTkwNTk1OTAxMTcxODUwNDI1ODQ3NjgxNzY0MzU2NTM1IiwiNDU2OTY3NjE1OTg3MjgwNDYwOTQzMzcyMTcxODAxNjc2MzE2NDczNTQwMzA5Njg4NjE1OTIxMTg0NjA1MDE3MDY1OTk1MTE3NjU4MSJdLFsiMSIsIjAiXV0sInBpX2MiOlsiMTc4ODM0NTM4NjIxNDI2ODI2MjUwNjI3MDA5NTEzMTU0ODQ4OTUyMDA0OTI3MDgwOTk4MzcwNzM1NjAyNzYxNzk4OTM5MzQ5NzQ2MjEiLCI3NzU4ODI2NjAwNTM2MDU3MDUwNTc2MDMxMDE4NjQ0MDk4NjQyODMxMTE5MzQ2ODM3NjgyMTMzNDU5MjgyMjg4NzExMjgyMzA2NjM4IiwiMSJdLCJwcm90b2NvbCI6Imdyb3RoMTYifSwicHViX3NpZ25hbHMiOlsiMTkyMjkwODQ4NzM3MDQ1NTAzNTcyMzI4ODcxNDI3NzQ2MDU0NDIyOTczMzcyMjkxNzY1NzkyMjkwMTEzNDIwOTE1OTQxNzQ5NzciLCI2MTEwNTE3NzY4MjQ5NTU5MjM4MTkzNDc3NDM1NDU0NzkyMDI0NzMyMTczODY1NDg4OTAwMjcwODQ5NjI0MzI4NjUwNzY1NjkxNDk0IiwiMTI0MzkwNDcxMTQyOTk2MTg1ODc3NDIyMDY0NzYxMDcyNDI3Mzc5ODkxODQ1Nzk5MTQ4NjAzMTU2NzI0NDEwMDc2NzI1OTIzOTc0NyJdfQ")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	var zkProof types.ZKProof
 	proofBytes, err := base64.RawURLEncoding.DecodeString("eyJwcm9vZiI6eyJwaV9hIjpbIjE5MTU5MDg5MTAwMDkzNDQyMzY0NTY0MjQxOTA3ODQ1MzkxODgxMzM5NDQ3NDkxNTcwNjg2NTk5NDE3MjA0MzUwNTE1ODE0NzYxNDE1IiwiNDQ4MDg2MzgzNDY4MTU2ODM2MTI2NTI1NzgzMzkyMjk1OTE1Mzg5OTQwNDUzMDkxNjcxNTA5NjEyMzg3NTU1MzY0NjM3NjMwNTQzOSIsIjEiXSwicGlfYiI6W1siMTA3MjY0OTYxNTk4OTQwNDAyNTExMDYyMDkyOTA5MjUzOTQ3MDU1MTk0NTYyNTkyMDYwNjgxMTE0MTY4ODQyMDI2MzI0MzY4Nzk1MDAiLCIzODkwMTY0OTc1OTMzOTQzMDY2NTc5ODI3OTk2MDcxNzI0NDg5NjEwNDU1ODQ0NTU5NDQ2MDIwMTk4ODQyNDQwNzk5MzAyNzQyOTk5Il0sWyIxOTY4NjI5MDk3ODAzMzI1MTU1MjczMjAzNTMxMzIyODYwNTE0Mzc3OTUwOTkwNTk1OTAxMTcxODUwNDI1ODQ3NjgxNzY0MzU2NTM1IiwiNDU2OTY3NjE1OTg3MjgwNDYwOTQzMzcyMTcxODAxNjc2MzE2NDczNTQwMzA5Njg4NjE1OTIxMTg0NjA1MDE3MDY1OTk1MTE3NjU4MSJdLFsiMSIsIjAiXV0sInBpX2MiOlsiMTc4ODM0NTM4NjIxNDI2ODI2MjUwNjI3MDA5NTEzMTU0ODQ4OTUyMDA0OTI3MDgwOTk4MzcwNzM1NjAyNzYxNzk4OTM5MzQ5NzQ2MjEiLCI3NzU4ODI2NjAwNTM2MDU3MDUwNTc2MDMxMDE4NjQ0MDk4NjQyODMxMTE5MzQ2ODM3NjgyMTMzNDU5MjgyMjg4NzExMjgyMzA2NjM4IiwiMSJdLCJwcm90b2NvbCI6Imdyb3RoMTYifSwicHViX3NpZ25hbHMiOlsiMTkyMjkwODQ4NzM3MDQ1NTAzNTcyMzI4ODcxNDI3NzQ2MDU0NDIyOTczMzcyMjkxNzY1NzkyMjkwMTEzNDIwOTE1OTQxNzQ5NzciLCI2MTEwNTE3NzY4MjQ5NTU5MjM4MTkzNDc3NDM1NDU0NzkyMDI0NzMyMTczODY1NDg4OTAwMjcwODQ5NjI0MzI4NjUwNzY1NjkxNDk0IiwiMTI0MzkwNDcxMTQyOTk2MTg1ODc3NDIyMDY0NzYxMDcyNDI3Mzc5ODkxODQ1Nzk5MTQ4NjAzMTU2NzI0NDEwMDc2NzI1OTIzOTc0NyJdfQ")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	err = json.Unmarshal(proofBytes, &zkProof)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	payloadBytes, err := base64.RawURLEncoding.DecodeString("bXltZXNzYWdl")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	assert.Equal(t, zkProof.PubSignals, token.ZkProof.PubSignals)
 	assert.Equal(t, zkProof.Proof, token.ZkProof.Proof)
@@ -114,21 +112,21 @@ func TestToken_Parse(t *testing.T) {
 func TestToken_ParseWithOutputs(t *testing.T) {
 
 	token, err := Parse("eyJhbGciOiJncm90aDE2IiwiY2lyY3VpdElkIjoiYXV0aFYyIiwiY3JpdCI6WyJjaXJjdWl0SWQiXSwidHlwIjoiSldaIn0.bXltZXNzYWdl.eyJwcm9vZiI6eyJwaV9hIjpbIjE5MTU5MDg5MTAwMDkzNDQyMzY0NTY0MjQxOTA3ODQ1MzkxODgxMzM5NDQ3NDkxNTcwNjg2NTk5NDE3MjA0MzUwNTE1ODE0NzYxNDE1IiwiNDQ4MDg2MzgzNDY4MTU2ODM2MTI2NTI1NzgzMzkyMjk1OTE1Mzg5OTQwNDUzMDkxNjcxNTA5NjEyMzg3NTU1MzY0NjM3NjMwNTQzOSIsIjEiXSwicGlfYiI6W1siMTA3MjY0OTYxNTk4OTQwNDAyNTExMDYyMDkyOTA5MjUzOTQ3MDU1MTk0NTYyNTkyMDYwNjgxMTE0MTY4ODQyMDI2MzI0MzY4Nzk1MDAiLCIzODkwMTY0OTc1OTMzOTQzMDY2NTc5ODI3OTk2MDcxNzI0NDg5NjEwNDU1ODQ0NTU5NDQ2MDIwMTk4ODQyNDQwNzk5MzAyNzQyOTk5Il0sWyIxOTY4NjI5MDk3ODAzMzI1MTU1MjczMjAzNTMxMzIyODYwNTE0Mzc3OTUwOTkwNTk1OTAxMTcxODUwNDI1ODQ3NjgxNzY0MzU2NTM1IiwiNDU2OTY3NjE1OTg3MjgwNDYwOTQzMzcyMTcxODAxNjc2MzE2NDczNTQwMzA5Njg4NjE1OTIxMTg0NjA1MDE3MDY1OTk1MTE3NjU4MSJdLFsiMSIsIjAiXV0sInBpX2MiOlsiMTc4ODM0NTM4NjIxNDI2ODI2MjUwNjI3MDA5NTEzMTU0ODQ4OTUyMDA0OTI3MDgwOTk4MzcwNzM1NjAyNzYxNzk4OTM5MzQ5NzQ2MjEiLCI3NzU4ODI2NjAwNTM2MDU3MDUwNTc2MDMxMDE4NjQ0MDk4NjQyODMxMTE5MzQ2ODM3NjgyMTMzNDU5MjgyMjg4NzExMjgyMzA2NjM4IiwiMSJdLCJwcm90b2NvbCI6Imdyb3RoMTYifSwicHViX3NpZ25hbHMiOlsiMTkyMjkwODQ4NzM3MDQ1NTAzNTcyMzI4ODcxNDI3NzQ2MDU0NDIyOTczMzcyMjkxNzY1NzkyMjkwMTEzNDIwOTE1OTQxNzQ5NzciLCI2MTEwNTE3NzY4MjQ5NTU5MjM4MTkzNDc3NDM1NDU0NzkyMDI0NzMyMTczODY1NDg4OTAwMjcwODQ5NjI0MzI4NjUwNzY1NjkxNDk0IiwiMTI0MzkwNDcxMTQyOTk2MTg1ODc3NDIyMDY0NzYxMDcyNDI3Mzc5ODkxODQ1Nzk5MTQ4NjAzMTU2NzI0NDEwMDc2NzI1OTIzOTc0NyJdfQ")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	outs := circuits.AuthV2PubSignals{}
 	err = token.ParsePubSignals(&outs)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	assert.Equal(t, "x4jcHP4XHTK3vX58AHZPyHE8kYjneyE6FZRfz7K29", outs.UserID.String())
 	assert.Equal(t, "4325bf7386b102c223cd6109e3b6b1bc813ecb14b2c3332bbd2aa7106e06c002", outs.GISTRoot.Hex())
 
 	msgHash, err := token.GetMessageHash()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, msgHash, outs.Challenge.Bytes())
 
 	did, err := core.ParseDIDFromID(*outs.UserID)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, "did:iden3:polygon:mumbai:x4jcHP4XHTK3vX58AHZPyHE8kYjneyE6FZRfz7K29", did.String())
 
 }
